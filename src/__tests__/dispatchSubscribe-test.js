3f049ea4b012eac501d3c271fc28a43ff21e8e38
slorber <lorber.sebastien@gmail.com>
2017-10-19 17:02:28 +0200

    Add test to ensure dispatching in listener is valid usecase

--- src/__tests__/dispatchSubscribe-test.js
+++ src/__tests__/dispatchSubscribe-test.js
@@ -13,7 +13,7 @@ function createStore() {
 }
 
 describe('batchedSubscribe()', () => {
-  it('it calls batch function on dispatch', () => {
+  it('it calls listeners function on dispatch', () => {
     const store = createStore();
     const listener1 = expect.createSpy();
     store.addDispatchListener(listener1);
@@ -31,5 +31,22 @@ describe('batchedSubscribe()', () => {
     store.dispatch({ type: 'bar' });
     expect(listener1.calls.length).toEqual(2);
     expect(listener2.calls.length).toEqual(2);
+  });
+
+  it('it can dispatch in listener', () => {
+    const store = createStore();
+    store.addDispatchListener(action => {
+      if (action.type === 'foo') {
+        store.dispatch({ type: 'bar' });
+      }
+    });
+
+    const listener = expect.createSpy();
+    store.addDispatchListener(listener);
+
+    store.dispatch({ type: 'first' });
+    expect(listener.calls.length).toEqual(1);
+    store.dispatch({ type: 'foo' });
+    expect(listener.calls.length).toEqual(3);
   });
 });
